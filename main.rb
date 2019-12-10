4deb3e7bfb0d3c6c3671ce7d39d9a794f24ff8ea
KOSaber <k.o.saber@hotmail.com>
2019-12-10 10:26:02 +0300

    done

--- main.rb
+++ main.rb
@@ -1,1 +1,33 @@
 #your code here
+
+
+
+def check (str1,str2)
+    distance = 0
+            str1.length.times do |index|
+    
+                if str1[index] == str2[index] then
+                    distance+=1
+                end
+    
+            end
+        return puts "#{distance}/4 correct "
+    end
+    
+    
+    
+    arr = ["book","look","luck"]
+    str2 = arr[1]
+    puts "Guess (4 left)?"
+    input = gets.chomp
+    check(input,str2)
+    puts "Guess (3 left)?"
+    input = gets.chomp
+    check(input,str2)
+    puts "Guess (2 left)?"
+    input = gets.chomp
+    check(input,str2)
+    puts "Guess (1 left)?"
+    input = gets.chomp
+    check(input,str2)
+    
